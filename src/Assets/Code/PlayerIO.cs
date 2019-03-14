5ef3525e7afb6fe2a3b966fd70100e186aba40d7
Mark Meijer <engineermarkpro@gmail.com>
2019-03-14 18:26:39 +0100

    Block Effect
    Little cubes are spawned when destroying a block

--- src/Assets/Code/PlayerIO.cs
+++ src/Assets/Code/PlayerIO.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UM;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,16 +20,42 @@ public class PlayerIO : MonoBehaviour
     [Header("UI")]
     [SerializeField] Text inventoryText;
 
+    //variables for the block break effect
+    private Queue<GameObject> blockbreakQueue; // Store multiple cubes so you dont need to intialize during the destruction, thus reducing performance hits
+    [Header("Block Destruction")]
+    [Tooltip("Amount to pool in order to save performance in runtime")]
+    [SerializeField] int preGeneration = 150;
+    [Tooltip("Amount of 'particles' that drop when destroying a block")]
+    [SerializeField] int effectAmount = 10;
+    GameObject effectParent;
+
     void Start()
     {
         singleton = this;
         cam = GetComponent<Camera>();
         inventory = new Inventory();
+        blockbreakQueue = new Queue<GameObject>();
+        // Create empty go for storing the physical objects in
+        effectParent = new GameObject("BlockEffectQueue");
+        for (int i = 0; i < preGeneration; i++)
+        {
+            GenerateNewParticle(effectParent.transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach(BlockParticle bp in effectParent.GetComponentsInChildren<BlockParticle>()){
+            if(bp.shouldDequeue){
+                bp.shouldDequeue = false;
+                GameObject go = bp.gameObject;
+                go.SetActive(false);
+                go.transform.position = new vec3(0).ToVector3();
+                blockbreakQueue.Enqueue(go);
+            }
+        }
+
         if (Input.GetKeyDown("1")) { inventory.Selected = 0; inventoryText.text = inventory.Stringify(); }
         else if (Input.GetKeyDown("2")) { inventory.Selected = 1; inventoryText.text = inventory.Stringify(); }
         else if (Input.GetKeyDown("3")) { inventory.Selected = 2; inventoryText.text = inventory.Stringify(); }
@@ -59,6 +86,11 @@ public class PlayerIO : MonoBehaviour
                 inventory.AddBlock(updateChunk.GetByte(p));
                 updateChunk.SetBrick(0, p);
                 inventoryText.text = inventory.Stringify();
+                vec3 v = vec3.ToVec3(p);
+                v.x += UnityEngine.Random.Range(-10,10)*0.1f;
+                v.y += UnityEngine.Random.Range(-10,10)*0.1f;
+                v.z += UnityEngine.Random.Range(-10,10)*0.1f;
+                GenerateBlockBreakEffect(v);
             }
             else if (inventory.HasBlock(inventory.Selected))
             {
@@ -73,5 +105,55 @@ public class PlayerIO : MonoBehaviour
             }
         }
         inventoryText.text = inventory.Stringify();
+    }
+
+    public GameObject GenerateNewParticle(Transform parent, bool enqueue = true){
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = Vector3.zero;
+        go.transform.localScale = new vec3(WorldGen.singleton.brickHeight * 0.08f).ToVector3();
+
+        go.transform.SetParent(parent.transform);
+
+        go.AddComponent<Rigidbody>();
+        go.AddComponent<BlockParticle>();
+
+        if (enqueue)
+        {
+            go.SetActive(false);
+            blockbreakQueue.Enqueue(go);
+        }
+
+        return go;
+    }
+
+    public void GenerateBlockBreakEffect(vec3 p){
+        for (int i = 0; i < effectAmount; i++)
+        {
+            GameObject go;
+            if (blockbreakQueue.Count > 1)
+                go = blockbreakQueue.Dequeue();
+            else
+                go = GenerateNewParticle(effectParent.transform,false);
+
+            go.transform.position = p.ToVector3();
+            go.SetActive(true);
+        }
+    }
+}
+
+// Block effect code
+public class BlockParticle : MonoBehaviour
+{
+    // Timer
+    private float timer = 0;
+    private float maxTimer = 10;
+    public bool shouldDequeue = false;
+
+    public void Update(){
+        timer += Time.deltaTime;
+        if(timer>maxTimer){
+            // Enqueue and disable
+            shouldDequeue = true;
+        }
     }
 }
---
+++ src/Assets/Code/UM.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Unity Mathematics is based on GLM, OpenGL Mathematics
+// Created as base math helper functions for Markcraft
+// Doesn't depend on anything but standard System namespaces and Unity classes for conversions
+
+namespace UM{
+    public struct vec3{
+        public float x;
+        public float y;
+        public float z;
+
+        public vec3(float x){
+            this.x = x;
+            this.y = x;
+            this.z = x;
+        }
+
+        public vec3(float x,float y,float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3 ToVector3(){
+            return ToVector3(this);
+        }
+
+        public static Vector3 ToVector3(vec3 v){
+            return new Vector3(v.x, v.y, v.z);
+        }
+
+        public static vec3 ToVec3(Vector3 v){
+            return new vec3(v.x, v.y, v.z);
+        }
+    }
+}
