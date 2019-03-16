22be4e9c5cbd7e4d6f07195fb66a8fe9df9d9a09
Mark Meijer <engineermarkpro@gmail.com>
2019-03-16 23:35:36 +0100

    vec3 comments
    Added IntelliSense supported comments to vec3

--- src/Assets/Code/UM.cs
+++ src/Assets/Code/UM.cs
@@ -15,6 +15,10 @@ namespace UM
         public float y;
         public float z;
 
+        /// <summary>
+        /// vec3 constructor with a single value that applies to all
+        /// </summary>
+        /// <param name="x"></param>
         public vec3(float x)
         {
             this.x = x;
@@ -22,6 +26,12 @@ namespace UM
             this.z = x;
         }
 
+        /// <summary>
+        /// vec3 constructor with all 3 values
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
         public vec3(float x, float y, float z)
         {
             this.x = x;
@@ -29,46 +39,84 @@ namespace UM
             this.z = z;
         }
 
+        /// <summary>
+        /// Converts the current object to Unity's Vector3
+        /// </summary>
+        /// <returns></returns>
         public Vector3 ToVector3()
         {
             return ToVector3(this);
         }
 
+        /// <summary>
+        /// Converts a custom vec3 to Unity's Vector3
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public static Vector3 ToVector3(vec3 v)
         {
             return new Vector3(v.x, v.y, v.z);
         }
 
+        /// <summary>
+        /// Convert a Unity Vector3 to custom vec3
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public static vec3 ToVec3(Vector3 v)
         {
             return new vec3(v.x, v.y, v.z);
         }
 
+        /// <summary>
+        /// Adds up 2 vectors and returns a new one
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static vec3 operator +(vec3 a, vec3 b)
         {
             return new vec3(a.x + b.x, a.y + b.y, a.z + b.z);
         }
 
-        public static vec3 operator +(vec3 v)
-        {
-            return new vec3() + v;
-        }
-
+        /// <summary>
+        /// Substracts 2 vectors from eachother and returns a new vector
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static vec3 operator -(vec3 a, vec3 b)
         {
             return new vec3(a.x - b.x, a.y - b.y, a.z - b.z);
         }
 
+        /// <summary>
+        /// Negate a vector
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public static vec3 operator -(vec3 v)
         {
             return new vec3() - v;
         }
 
+        /// <summary>
+        /// Multiplies 2 vectors into a new vector
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static vec3 operator *(vec3 a, vec3 b)
         {
             return new vec3(a.x * b.x, a.y * b.y, a.z * b.z);
         }
 
+        /// <summary>
+        /// Divides 2 vectors and returns a new vector
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static vec3 operator /(vec3 a, vec3 b)
         {
             return new vec3(a.x / b.x, a.y / b.y, a.z / b.z);
